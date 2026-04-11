1cb2d41dcd76334b681b3f5be2e116d407c4c201
bongacat07 <vaibhavanandeh07@gmail.com>
2026-04-11 10:08:17 +0530

    TCB implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use tun_rs::DeviceBuilder;
-
+use rand::Rng;
 use tcp::{
     parser, tcp_parser, ip_checksum, tcp_checksum, create_packet,
     Packet, Ipv4Packet, Ipv6Header, TCPPacket,
@@ -7,6 +7,7 @@ use tcp::{
 };
 use std::collections::HashMap;
 use std::collections::HashSet;
+
 
 fn print_ipv4(h: &Ipv4Packet) {
     println!("--- IPv4 Packet ---");
@@ -51,7 +52,6 @@ fn main() {
         .build_sync()
         .unwrap();
 
-    let mut state = TCPState::Closed;
     let mut buf = [0u8; 65535];
     let mut connections: HashMap<ConnectionKey, TCB> = HashMap::new();
     let mut listener: HashSet<u16> = HashSet::new();
@@ -75,83 +75,107 @@ fn main() {
                         if let Some(tcp) = packet {
                             print_tcp(&tcp);
 
-                            if !listener.contains(&tcp.header.dst_port){
-                                print!("RST implementation do be done");
-                                continue;
-                            }
-                            let iss = 100;
 
-                            connections.insert(
-                                ConnectionKey {
-                                    src_ip: h.header.fields.source,
-                                    src_port: tcp.header.src_port,
-                                    dst_ip: h.header.fields.destination,
-                                    dst_port: tcp.header.dst_port,
-                                },
-                                TCB {
-                                    state: TCPState::Listen,
 
-                                    iss,
-                                    snd_una: iss,
-                                    snd_nxt: iss + 1,
+                            let key = ConnectionKey{
+                                src_ip: h.header.fields.source,
+                                src_port: tcp.header.src_port,
+                                dst_ip:h.header.fields.destination,
+                                dst_port:tcp.header.dst_port
 
-                                    irs: tcp.header.seq_num,
-                                    rcv_nxt: tcp.header.seq_num + 1,
+                            };
+                            if let Some(tcb) = connections.get_mut(&key) {
+                                let flags = tcp.header.flags;
+
+                                match tcb.state {
+                                    TCPState::SynReceived => {
+                                        if (flags & 0x10) != 0 && (flags & 0x02) == 0 {
+                                            if tcp.header.ack_num == tcb.snd_nxt {
+                                                tcb.state = TCPState::Established;
+                                                tcb.snd_una = tcp.header.ack_num;
+
+                                                println!("Handshake complete");
+                                            } else {
+                                                println!("Invalid ACK");
+                                            }
+                                        }
+                                    }
+
+                                    _ => {}
                                 }
-                            );
-                            let flags = tcp.header.flags;
-                            if (flags & 0x02) != 0 && (flags & 0x10) == 0 {
-                                let recv_ip = &h.header.fields;
-                                let recv_tcp = &tcp.header;
-
-                                let mut tcp_packet = TCPPacket {
-                                    header: TCPHeader {
-                                        src_port: recv_tcp.dst_port,
-                                        dst_port: recv_tcp.src_port,
-                                        seq_num: 0,
-                                        ack_num: recv_tcp.seq_num + 1,
-                                        data_offset: 5,
-                                        flags: 0x12,
-                                        window: 64240,
-                                        checksum: 0,
-                                        urgent_ptr: 0,
-                                    },
-                                    payload: vec![],
-                                };
-
-                                let ip_fields = Ipv4HeaderFields {
-                                    version: 4,
-                                    ihl: 5,
-                                    tos: 0,
-                                    total_length: 40,
-                                    identification: 0,
-                                    flags: 0,
-                                    fragment_offset: 0,
-                                    ttl: 64,
-                                    protocol: 6,
-                                    source: recv_ip.destination,
-                                    destination: recv_ip.source,
-                                };
-
-                                let ip_chk = ip_checksum(&ip_fields);
-                                let tcp_chk = tcp_checksum(
-                                    recv_ip.destination,
-                                    recv_ip.source,
-                                    &tcp_packet,
-                                );
-                                tcp_packet.header.checksum = tcp_chk;
-
-                                let ip_header = Ipv4Header {
-                                    fields: ip_fields,
-                                    header_checksum: ip_chk,
-                                };
-
-                                let packet = create_packet(&tcp_packet, &ip_header);
-                                dev.send(&packet);
-
-
-                                println!("SYN received, SYN-ACK sent");
                             }
+                            else{
+                                if !listener.contains(&tcp.header.dst_port){
+                                    print!("RST implementation do be done");
+                                    continue;
+                                }
+                                let flags = tcp.header.flags;
+                                if (flags & 0x02) != 0 && (flags & 0x10) == 0 {
+                                    let recv_ip = &h.header.fields;
+                                    let recv_tcp = &tcp.header;
+                                    let iss: u32 = 1000; //for now
+                                    let mut tcp_packet = TCPPacket {
+                                        header: TCPHeader {
+                                            src_port: recv_tcp.dst_port,
+                                            dst_port: recv_tcp.src_port,
+                                            seq_num: iss,
+                                            ack_num: recv_tcp.seq_num + 1,
+                                            data_offset: 5,
+                                            flags: 0x12,
+                                            window: 64240,
+                                            checksum: 0,
+                                            urgent_ptr: 0,
+                                        },
+                                        payload: vec![],
+                                    };
+
+                                    let ip_fields = Ipv4HeaderFields {
+                                        version: 4,
+                                        ihl: 5,
+                                        tos: 0,
+                                        total_length: 40,
+                                        identification: 0,
+                                        flags: 0,
+                                        fragment_offset: 0,
+                                        ttl: 64,
+                                        protocol: 6,
+                                        source: recv_ip.destination,
+                                        destination: recv_ip.source,
+                                    };
+
+                                    let ip_chk = ip_checksum(&ip_fields);
+                                    let tcp_chk = tcp_checksum(
+                                        recv_ip.destination,
+                                        recv_ip.source,
+                                        &tcp_packet,
+                                    );
+                                    tcp_packet.header.checksum = tcp_chk;
+
+                                    let ip_header = Ipv4Header {
+                                        fields: ip_fields,
+                                        header_checksum: ip_chk,
+                                    };
+                                    connections.insert(
+                                     key,
+                                        TCB {
+                                            state: TCPState::SynReceived,
+
+                                            iss,
+                                            snd_una: iss,
+                                            snd_nxt: iss + 1,
+
+                                            irs: tcp.header.seq_num,
+                                            rcv_nxt: tcp.header.seq_num + 1,
+                                        }
+                                    );
+                                    let packet = create_packet(&tcp_packet, &ip_header);
+                                    dev.send(&packet);
+
+
+                                    println!("SYN received, SYN-ACK sent");
+                                }
+                            }
+
 
 
                         }
